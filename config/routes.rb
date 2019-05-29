653fa7669039ddf9ad5d55a69e957c460692679a
Jon <jonwalsh539@gmail.com>
2019-05-29 15:39:51 +0000

    Fixed layout

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'static_pages/living'
   get 'static_pages/fun'
   # For details on the DraiSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#hello'
+  root 'static_pages#home'
 end
